2c01160762054a25a77200335c3a8053d6428f11
Ata Mahmoudi <ata.mahmoudi@gmail.com>
2025-09-11 22:52:11 +0000

    feat: add /healthz and /readyz endpoints

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthController
+{
+    #[Route('/healthz', name: 'healthz', methods: ['GET'])]
+    public function healthz(): JsonResponse
+    {
+        // Liveness: process is up
+        return new JsonResponse(['status' => 'ok']); // 200 OK
+    }
+
+    #[Route('/readyz', name: 'readyz', methods: ['GET'])]
+    public function readyz(): JsonResponse
+    {
+        // Readiness: application is ready to serve traffic
+        $checks = [
+            'php_fpm' => true,
+        ];
+        $ready = !in_array(false, $checks, true);
+
+        return new JsonResponse([
+            'ready' => $ready,
+            'checks' => $checks,
+        ], $ready ? 200 : 503);
+    }
+}
