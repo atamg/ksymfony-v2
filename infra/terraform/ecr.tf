0e09d812170a2215b0b37441b9c17b4816235bce
Ata Mahmoudi <ata.mahmoudi@gmail.com>
2025-09-14 12:48:53 +0000

    fix(cd): add missing variables for sg

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "app" {
   name = var.project_name
   image_scanning_configuration { scan_on_push = true }
   image_tag_mutability         = "MUTABLE"
+  force_delete = true
 }
 
 resource "aws_ecr_lifecycle_policy" "cleanup" {
